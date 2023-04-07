eddc93936bca1871c7299f800a22bb2721be265d
Raphael Duarte <raphaelduartessph@gmail.com>
2023-04-07 13:59:10 -0300

    Mudanca do diretorio

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using InvestigationJob.Models.Interface;
+using InvestigationJob.Models.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InvestigationJob.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Service/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using InvestigationJob.Models.Interface;
+
+namespace InvestigationJob.Models.Service
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<UserModel> GetAllUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
